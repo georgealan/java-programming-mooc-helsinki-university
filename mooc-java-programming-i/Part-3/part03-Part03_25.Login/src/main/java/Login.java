6fa17c2b59b0e5ec8f239fd9cd98fd6d1db67011
George Alan <georgealanrufo@gmail.com>
2022-10-13 17:28:40 -0300

    Continue in exercise part03_26.LineByLine

--- mooc-java-programming-i/Part-3/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/Part-3/part03-Part03_25.Login/src/main/java/Login.java
@@ -1,12 +1,13 @@
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        HashMap<String, String> users = new HashMap<>();
+        users.put("sunshine", "alex");
+        users.put("haskell", "emma");
 
         System.out.println("Enter username:");
         String userName = scanner.nextLine();
@@ -14,6 +15,11 @@ public class Login {
         System.out.println("Enter password:");
         String password = scanner.nextLine();
 
-        
+        if (users.containsValue(userName) && users.containsKey(password)) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+
     }
 }
--- mooc-java-programming-i/Part-3/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/Part-3/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -1,11 +1,20 @@
-
 import java.util.Scanner;
 
 public class LineByLine {
+    static String phrase;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
 
+
+        }
+
+        String[] words = phrase.split(" ");
+
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
