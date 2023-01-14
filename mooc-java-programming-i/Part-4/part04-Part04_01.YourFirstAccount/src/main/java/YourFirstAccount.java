95047cc8598b878d466ef48ff9ec7cacbbc36d66
George Alan <georgealanrufo@gmail.com>
2023-01-14 16:45:33 -0300

    Part 4 exercise 15.NumbersStatistics

--- mooc-java-programming-i/Part-4/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/Part-4/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account georgeAccount = new Account("George", 100.0);
+        georgeAccount.deposit(20.0);
+        System.out.println(georgeAccount);
     }
 }
