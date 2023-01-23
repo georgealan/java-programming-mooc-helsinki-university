a43c18c7a1aee1ee779cbf52abde8846f27d2f74
George Alan <georgealanrufo@gmail.com>
2023-01-22 22:53:38 -0300

    Continue in exercise Part04 25

--- mooc-java-programming-i/Part-4/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-i/Part-4/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -1,10 +1,19 @@
 
 import java.util.Scanner;
-
+import java.lang.Math.*;
 public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String text = scanner.next();
+            if (text.equals("end")) {
+                break;
+            }
 
+            int number = Integer.parseInt(text);
+            int result = (int) Math.pow(number, 3);
+            System.out.println(result);
+        }
     }
 }
--- mooc-java-programming-i/Part-4/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ mooc-java-programming-i/Part-4/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -3,9 +3,19 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class PrintingASpecifiedFile {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Which file should have its contents printed?");
+        String path = scanner.nextLine();
+
+        try (Scanner file = new Scanner(Paths.get(path))) {
+            while (file.hasNextLine()) {
+                String row = scanner.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
